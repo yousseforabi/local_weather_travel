3b105d2c3380fa5add5743351ce9880ae8f76167
Carmelo85S <carmelo.salis85@gmail.com>
2025-03-03 11:25:19 +0100

    init server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const corsOption = {
+    origin:["http://localhost:5173"]
+};
+
+app.use (cors(corsOption));
+
+
+
+app.listen(8080,() => {
+    console.log("server running on port 8080")
+})
